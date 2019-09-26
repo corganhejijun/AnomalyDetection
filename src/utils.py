3954d1b93f9a7b073aa3d2a30215ae49f5aaeed8
corganhejijun <corganhejijun@gmail.com>
2019-09-26 13:27:27 +0800

    mask error

--- src/utils.py
+++ src/utils.py
@@ -46,10 +46,13 @@ def load_testdata(data, fine_size):
             img_A = img
             img_A[0:step, 0:step] = -1 
             list.append(np.dstack((img_A, img)))
+            img_A = img
             img_A[0:step, step+1:] = -1
             list.append(np.dstack((img_A, img)))
+            img_A = img
             img_A[step+1:, 0:step] = -1
             list.append(np.dstack((img_A, img)))
+            img_A = img
             img_A[step+1:, step+1:] = -1
             list.append(np.dstack((img_A, img)))
             y += step
